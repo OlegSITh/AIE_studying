ee5088a6fcc5de775a672b8dece42c8d4b91d96d
OlegSITh <oleg.s.it@mail.ru>
2025-12-19 23:47:24 +0300

    Обновлена третья домашка

--- homeworks/HW03/eda-cli/src/eda_cli/cli.py
+++ homeworks/HW03/eda-cli/src/eda_cli/cli.py
@@ -106,7 +106,7 @@ def report(
     md_path = out_root / "report.md"
     with md_path.open("w", encoding="utf-8") as f:
 
-        f.write(f"# EDA-отчёт\n\n")
+        f.write(f"{title}\n\n")
         f.write(f"Исходный файл: `{Path(path).name}`\n\n")
         f.write(f"Строк: **{summary.n_rows}**, столбцов: **{summary.n_cols}**\n\n")
 
